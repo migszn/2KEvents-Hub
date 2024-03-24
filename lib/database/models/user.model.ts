d87605c37aeb764a2be9a89cd1ac50b4a7a97ddf
migslicsi <106062492+migslicsi@users.noreply.github.com>
2024-03-24 12:47:41 +0800

    added firstname in schema

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -4,6 +4,7 @@ const UserSchema = new Schema ({
     clerkId: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     username: { type: String, required: true, unique: true },
+    firstName: { type: String, required: true },
     lastname: { type: String, required: true},
     photo: { type: String, required: true},
 })
